1ee6d564141479dcbffd2b63cd31adceca617630
tyrex1829 <sakshamkhare19@gmail.com>
2024-07-30 20:11:36 +0530

    useState logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [networkCount, setNetworkCount] = useState(0);
+  const [jobCount, setJobCount] = useState(0);
+  const [messagingCount, setMessagingCount] = useState(0);
+  const [notificationCount, setNotificationCount] = useState(0);
 
   return (
     <>
       <button>Home</button>
 
-      <button>My network ()</button>
-      <button>Jobs ()</button>
-      <button>Messaging ()</button>
-      <button>Notification ()</button>
+      <button>My network ({networkCount})</button>
+      <button>Jobs ({jobCount})</button>
+      <button>Messaging ({messagingCount})</button>
+      <button>Notification ({notificationCount})</button>
 
       <button>Me</button>
     </>
