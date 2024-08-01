df8f6f271cf1d5512fdb1495af8666ccfe482f70
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-01 17:46:42 +0530

    useRecoilStateLoadable

--- src/Appz.jsx
+++ src/Appz.jsx
@@ -1,6 +1,11 @@
 // code for atom family, here rendering todos.
 import "./App.css";
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilStateLoadable,
+  useRecoilValue,
+} from "recoil";
 import { todosAtomFamily } from "./atomsz";
 
 function Appz() {
@@ -13,15 +18,23 @@ function Appz() {
 }
 
 function Todo({ id }) {
-  const currentTodo = useRecoilValue(todosAtomFamily(id));
-
-  return (
-    <>
-      <h2>{currentTodo.title}</h2>
-      <p>{currentTodo.description}</p>
-      <br />
-    </>
+  const [currentTodo, setCurrentTodo] = useRecoilStateLoadable(
+    todosAtomFamily(id)
   );
+
+  if (currentTodo.state === "loading") {
+    return <h2>"loading..."</h2>;
+  } else if (currentTodo.state === "hasValue") {
+    return (
+      <>
+        <h2>{currentTodo.contents.title}</h2>
+        <p>{currentTodo.contents.description}</p>
+        <br />
+      </>
+    );
+  } else if (currentTodo.state === "hasError") {
+    return <div>Error while getting data from backend</div>;
+  }
 }
 
 export default Appz;
