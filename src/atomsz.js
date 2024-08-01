8b954abd881b2348a6d2be1934429191b3ecdd61
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-01 17:19:43 +0530

    selector family code

--- src/atomsz.js
+++ src/atomsz.js
@@ -1,9 +1,27 @@
-import { atomFamily } from "recoil";
+import { atomFamily, selectorFamily } from "recoil";
 import { TODOS } from "./todos";
+import axios from "axios";
 
+// for hard coded todo, atom family
+// export const todosAtomFamily = atomFamily({
+//   key: "todosAtomFamily",
+//   default: (id) => {
+//     return TODOS.find((x) => x.id === id);
+//   },
+// });
+
+// fetch from beckend, selector family
 export const todosAtomFamily = atomFamily({
   key: "todosAtomFamily",
-  default: (id) => {
-    return TODOS.find((x) => x.id === id);
-  },
+  default: selectorFamily({
+    key: "todoSelectorFamily",
+    get:
+      (id) =>
+      async ({ get }) => {
+        const res = await axios.get(
+          `https://sum-server.100xdevs.com/todo?id=${id}`
+        );
+        return res.data.todo;
+      },
+  }),
 });
